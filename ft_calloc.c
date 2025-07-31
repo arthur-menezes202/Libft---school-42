8523a1d1e9b146c804fc2dff03351a677c085758
arthur-menezes202 <arthurmcorrea282@gmail.com>
2025-07-31 10:59:45 -0300

    subindo o projeto

---
+++ ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armeneze <armeneze@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 11:54:14 by armeneze          #+#    #+#             */
+/*   Updated: 2025/07/31 10:46:05 by armeneze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void		*result;
+
+	if (size && nmemb && nmemb != nmemb * size / size)
+		return (0);
+	if (size == 0 || nmemb == 0)
+		ft_calloc(1, 1);
+	result = malloc(nmemb * size);
+	if (!result)
+		return (0);
+	ft_bzero(result, nmemb * size);
+	return (result);
+}
