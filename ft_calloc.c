d19266e26cc513dd31ac05bbc3004796de5ade70
arthur-menezes202 <arthurmcorrea282@gmail.com>
2025-07-31 11:03:09 -0300

    consertando calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: armeneze <armeneze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 11:54:14 by armeneze          #+#    #+#             */
-/*   Updated: 2025/07/31 10:46:05 by armeneze         ###   ########.fr       */
+/*   Updated: 2025/07/31 11:02:14 by armeneze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 	if (size && nmemb && nmemb != nmemb * size / size)
 		return (0);
-	if (size == 0 || nmemb == 0)
-		ft_calloc(1, 1);
 	result = malloc(nmemb * size);
 	if (!result)
 		return (0);
